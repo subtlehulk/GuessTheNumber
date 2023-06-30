170388d143e64b2b612dca0a3550294d465b5e61
Corey <coreygraham@live.co.uk>
2023-06-30 17:17:04 +0100

    Add player options

--- Program.cs
+++ Program.cs
@@ -27,27 +27,82 @@ class Program
         
     }
 
-    private static void PlayGame() { 
-        //Print the prompt to screen
-        System.Console.WriteLine("Pick a number between 1-10.");
-
-        //create a new instance of Random
-        Random ran = new Random();
-        //store the random number in an int
-        int number = ran.Next(1,11);
-
-        //Take input from the user
+    private static void PlayGame() {
+        //Ask for the number of players, between 1 and 4
+        System.Console.WriteLine("Please enter how many players there are, between 1 - 4.");
         string input = Console.ReadLine();
-        //convert the input to an int and store it
-        int guess = Convert.ToInt32(input);
-        ////Check the input against the random number
-        if (guess == number) {
-            System.Console.WriteLine("Congrats, you guess it correctly. The number was {0}", number);
+        int numOfPlayers = Convert.ToInt32(input);
+        if (numOfPlayers == 1) {
+            //Create one instance of Player
+            Player newPlayer1 = new Player("Player 1"); 
+
+            //Print the prompt to screen
+            System.Console.WriteLine("Pick a number between 1-10.");
+
+            //create a new instance of Random
+            Random ran = new Random();
+            //store the random number in an int
+            int number = ran.Next(1,11);
+            //Take input from the user
+            input = Console.ReadLine();
+            //convert the input to an int and store it
+            int guess = Convert.ToInt32(input);
+            ////Check the input against the random number
+            if (guess == number) {
+                System.Console.WriteLine("Congrats, you guess it correctly. The number was {0}", number);
+            }
+            else {
+                System.Console.WriteLine("Ugh, better luck next time!");
+                System.Console.WriteLine("The number was {0}", number);
+            }
+
+        }
+        else if (numOfPlayers == 2) {
+            //Create two instances of Player
+            Player newPlayer1 = new Player("Player 1");
+            Player newPlayer2 = new Player("Player 2");
+            
+
+             //create a new instance of Random
+            Random ran = new Random();
+            //store the random number in an int
+            int number = ran.Next(1,11);
+
+            System.Console.WriteLine("Player 1: ");
+            input = Console.ReadLine();
+            newPlayer1.guess = Convert.ToInt32(input);
+            System.Console.WriteLine("Player 2: ");
+            input = Console.ReadLine();
+            newPlayer2.guess = Convert.ToInt32(input);
+
+            int p1Diff = number - newPlayer1.guess; 
+            int p2Diff = number - newPlayer2.guess; 
+
+            System.Console.WriteLine("The number was {0}.", number);
+            if (p1Diff < p2Diff) {
+                System.Console.WriteLine("Player 1 is closest!");
+            }
+            else if (p1Diff > p2Diff) {
+                System.Console.WriteLine("Player 2 is closest!");
+            }
+            
+        }
+        else if (numOfPlayers == 3) {
+            //create three
+            Player newPlayer1 = new Player("Player 1");
+            Player newPlayer2 = new Player("Player 2");
+            Player newPlayer3 = new Player("Player 3");
+        }
+        else if (numOfPlayers == 4) {
+            //create four instances
+            Player newPlayer1 = new Player("Player 1");
+            Player newPlayer2 = new Player("Player 2");
+            Player newPlayer3 = new Player("Player 3");
+            Player newPlayer4 = new Player("Player 4");
 
         }
         else {
-            System.Console.WriteLine("Ugh, better luck next time!");
-            System.Console.WriteLine("The number was {0}", number);
+            System.Console.WriteLine("That's not a valid choice.");
         }
         
     }
